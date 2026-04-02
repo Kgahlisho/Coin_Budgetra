b0a87f4d147b98cf064e08bbbd97a4c11db9ee85
Kgahlisho <st10325173@rcconnect.edu.za>
2026-04-02 19:31:01 +0200

    commit: created other views

--- app/src/main/java/com/example/coin_budgetra/Dashboard_Module.kt
+++ app/src/main/java/com/example/coin_budgetra/Dashboard_Module.kt
@@ -1,10 +1,13 @@
 package com.example.coin_budgetra
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+
 
 class Dashboard_Module : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,6 +20,29 @@ class Dashboard_Module : AppCompatActivity() {
             insets
         }
 
+        val btnExpense = this.findViewById<Button>(R.id.button5)
+        btnExpense.setOnClickListener {
+            val intent = Intent(this, Expense_Module::class.java)
+            startActivity(intent);
+        }
 
+        val btnFinanace = this.findViewById<Button>(R.id.button6)
+        btnFinanace.setOnClickListener {
+            val intent = Intent(this, Finance_Module::class.java)
+            startActivity(intent);
+        }
+
+
+        val btnOther = this.findViewById<Button>(R.id.button7)
+        btnOther.setOnClickListener {
+            val intent = Intent(this, Expense_Module::class.java)
+            startActivity(intent);
+        }
+
+        val btnChallenge = this.findViewById<Button>(R.id.button8)
+        btnChallenge.setOnClickListener {
+            val intent = Intent(this, Challenges_Module::class.java)
+            startActivity(intent);
+        }
     }
 }

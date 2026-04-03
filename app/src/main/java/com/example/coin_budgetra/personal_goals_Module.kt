277236359f7ad476756f024092ea1a46e5de3cf6
Kgahlisho <st10325173@rcconnect.edu.za>
2026-04-03 18:49:59 +0200

    commit : created the personal goals module and added a back button back to the dashnoard

---
+++ app/src/main/java/com/example/coin_budgetra/personal_goals_Module.kt
@@ -0,0 +1,31 @@
+package com.example.coin_budgetra
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class personal_goals_Module : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_personal_goals_module)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+
+        val btnBackdash = this.findViewById<Button>(R.id.button16)
+
+        btnBackdash.setOnClickListener{
+        val intent = Intent(this, Dashboard_Module::class.java)
+        startActivity(intent);
+
+        }
+    }
+}

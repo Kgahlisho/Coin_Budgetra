6c67d9dadb48f0fda5d9a9a1b26f10356adb5e79
Kgahlisho <st10325173@rcconnect.edu.za>
2026-04-04 18:00:26 +0200

    Added linking on the pages

--- app/src/main/java/com/example/coin_budgetra/personal_goals_Module.kt
+++ app/src/main/java/com/example/coin_budgetra/personal_goals_Module.kt
@@ -1,8 +1,11 @@
 package com.example.coin_budgetra
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ProgressBar
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -17,6 +20,8 @@ class personal_goals_Module : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+
+
         }
 
         val btnBackdash = this.findViewById<Button>(R.id.button16)
@@ -27,11 +32,38 @@ class personal_goals_Module : AppCompatActivity() {
 
         }
 
+
+
         val btnCancell = this.findViewById<Button>(R.id.button18)
 
         btnCancell.setOnClickListener {
             val intent = Intent(this, Dashboard_Module::class.java)
             startActivity(intent);
         }
+
+
     }
+
+
+override fun onActivityResult(requestCode: Int , resultCode:Int ,data: Intent?){
+    super.onActivityResult(requestCode,resultCode,data)
+
+
+if (requestCode ==1 && resultCode == Activity.RESULT_OK && data !=null) {
+
+    val name = data.getStringExtra("goalName")
+    val amount = data.getStringExtra("goalAmount")
+
+    val txtGoalName = this.findViewById<TextView>(R.id.txtGoalName)
+    val txtGoalTarget = this.findViewById<TextView>(R.id.txtGoalTarget)
+    val progressBar = this.findViewById<ProgressBar>(R.id.progressGoal)
+    val txtProgress = this.findViewById<TextView>(R.id.txtProgress)
+
+    txtGoalName.text = name
+    txtGoalTarget.text = "Target: R$amount"
+
+    progressBar.progress = 0
+    txtProgress.text = "R0 saved"
+}
+}
 }

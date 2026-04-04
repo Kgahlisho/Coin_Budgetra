5581bb53fe7bd0655e6e288145a8330f8fc3e2d6
Kgahlisho <st10325173@rcconnect.edu.za>
2026-04-04 11:19:40 +0200

    Created back buttons for the Pages and linked most of the pages together to and from navigation is possible.

--- app/src/main/java/com/example/coin_budgetra/Finance_Module.kt
+++ app/src/main/java/com/example/coin_budgetra/Finance_Module.kt
@@ -1,6 +1,8 @@
 package com.example.coin_budgetra
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,13 @@ class Finance_Module : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val btnBackd = this.findViewById<Button>(R.id.button17)
+
+        btnBackd.setOnClickListener{
+            val intent = Intent(this, Dashboard_Module::class.java)
+            startActivity(intent);
         }
     }
 }

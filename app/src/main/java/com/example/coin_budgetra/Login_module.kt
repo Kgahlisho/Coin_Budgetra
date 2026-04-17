7c72ed78da2a3d9e1ce0bfa1a1146bf5c8cc2fd6
Kgahlisho <st10325173@rcconnect.edu.za>
2026-04-18 00:52:08 +0200

    Implemented the Validation feature and logic

--- app/src/main/java/com/example/coin_budgetra/Login_module.kt
+++ app/src/main/java/com/example/coin_budgetra/Login_module.kt
@@ -3,12 +3,16 @@ package com.example.coin_budgetra
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class Login_module : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -19,19 +23,66 @@ class Login_module : AppCompatActivity() {
             insets
         }
 
+        val emailInput = findViewById<EditText>(R.id.editTextText)
+        val passwordInput = findViewById<EditText>(R.id.editTextText2)
+
 
         val CompleteLogin = this.findViewById<Button>(R.id.button);
 
         CompleteLogin.setOnClickListener {
-            val intent = Intent(this, Dashboard_Module::class.java)
-            startActivity(intent);
+
+            val email = emailInput.text.toString().trim()
+            val password = passwordInput.text.toString().trim()
+
+
+
+            if (email.isEmpty()) {
+                emailInput.error = "Email is required"
+                emailInput.requestFocus()
+                return@setOnClickListener
+            }
+            if (!android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                emailInput.error = "Enter a valid email"
+                emailInput.requestFocus()
+                return@setOnClickListener
+            }
+
+            if (password.isEmpty()) {
+                passwordInput.error = "Password is required"
+                passwordInput.requestFocus()
+                return@setOnClickListener
+            }
+
+            if (password.length < 8) {
+                passwordInput.error = "Password must be at least 8 characters"
+                passwordInput.requestFocus()
+                return@setOnClickListener
+            }
+
+            val user = UserRepository.users.find {
+                it.email == email && it.password == password
+            }
+
+            if (user == null) {
+                passwordInput.error = "Invalid email or password"
+                passwordInput.requestFocus()
+                return@setOnClickListener
+            }
+
+            UserRepository.currentUser = user
+
+
+            Toast.makeText(this, "Login successful! ", Toast.LENGTH_SHORT).show()
+
+            startActivity(Intent(this, Dashboard_Module::class.java))
+            finish()
         }
 
-        val GoSignup = this.findViewById<Button>(R.id.button3);
-
-        GoSignup.setOnClickListener {
-            val intent = Intent(this, Signup_module::class.java)
-            startActivity(intent);
+        findViewById<Button>(R.id.button3).setOnClickListener {
+            startActivity(Intent(this, Signup_module::class.java))
+            finish()
         }
+
+
     }
 }
--- app/src/main/java/com/example/coin_budgetra/Signup_module.kt
+++ app/src/main/java/com/example/coin_budgetra/Signup_module.kt
@@ -3,29 +3,125 @@ package com.example.coin_budgetra
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class Signup_module : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+
         setContentView(R.layout.activity_signup_module)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+
+
         }
 
 
-        val  CompleteSignupBtn = this.findViewById<Button>(R.id.button4);
 
-        CompleteSignupBtn.setOnClickListener{
+        val nameInput = findViewById<EditText>(R.id.editTextText3)
+        val surnameInput = findViewById<EditText>(R.id.editTextText4)
+        val phoneInput = findViewById<EditText>(R.id.editTextText5)
+        val emailInput = findViewById<EditText>(R.id.editTextText6)
+        val passwordInput = findViewById<EditText>(R.id.editTextText7)
+        val confirmPasswordInput = findViewById<EditText>(R.id.editTextText8)
+
+        findViewById<TextView>(R.id.txtGoToLogin).setOnClickListener {
+            startActivity(Intent(this, Login_module::class.java))
+        }
+
+        val CompleteSignUp = findViewById<Button>(R.id.button4)
+        CompleteSignUp.setOnClickListener {
+
+
+            val name = nameInput.text.toString().trim()
+            val surname = surnameInput.text.toString().trim()
+            val phone = phoneInput.text.toString().trim()
+            val email = emailInput.text.toString().trim()
+            val password = passwordInput.text.toString().trim()
+            val confirmPassword = confirmPasswordInput.text.toString().trim()
+
+            if (name.isEmpty()){
+                nameInput.error = "name is required"
+                nameInput.requestFocus()
+                return@setOnClickListener
+            }
+
+            if (surname.isEmpty()){
+                surnameInput.error = "surname is required"
+                surnameInput.requestFocus()
+                return@setOnClickListener
+            }
+
+            if (phone.isEmpty()){
+                phoneInput.error = "Phone number is required"
+                phoneInput.requestFocus()
+                return@setOnClickListener
+            }
+
+            if (phone.length < 10 ){
+                phoneInput.error = "Enter a valid cell phone number"
+                phoneInput.requestFocus()
+                return@setOnClickListener
+            }
+
+            if (!android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+                emailInput.error = "Enter a valid email adress"
+                emailInput.requestFocus()
+                return@setOnClickListener
+            }
+
+            if(password.length < 8){
+                passwordInput.error = "Password must be at least 8 Characters"
+                passwordInput.requestFocus()
+                return@setOnClickListener
+            }
+
+            if(password != confirmPassword){
+                confirmPasswordInput.error = "Passwords do not match"
+                confirmPasswordInput.requestFocus()
+                return@setOnClickListener
+            }
+
+            //we then prevent duplicate emails when user signs up
+            if (UserRepository.users.any { it.email == email}){
+                emailInput.error = "Email already registered"
+                emailInput.requestFocus()
+                return@setOnClickListener
+            }
+
+            //creates new user
+            val newUser = User(
+                name,
+                surname,
+                phone,
+                email,
+                password,
+            )
+            UserRepository.users.add(newUser)
+
+          Toast.makeText(this , "Sign up Complete! Please Login." , Toast.LENGTH_SHORT)
+
             val intent = Intent(this, Login_module::class.java)
-            startActivity(intent);
+            startActivity(intent)
+
         }
+
+
+
+
+
 
     }
 }

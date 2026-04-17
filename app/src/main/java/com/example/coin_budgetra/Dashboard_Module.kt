9e7791fb6cfec7b5154159a6139c31fea00d6d6c
Student(Kgahlisho) <mokoalakgahlisho@gmail.com>
2026-04-17 14:46:30 +0200

    the dashaboard has the accumilated amounts from the pages:

--- app/src/main/java/com/example/coin_budgetra/Dashboard_Module.kt
+++ app/src/main/java/com/example/coin_budgetra/Dashboard_Module.kt
@@ -2,7 +2,9 @@ package com.example.coin_budgetra
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.MotionEvent
 import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,10 +12,35 @@ import androidx.core.view.WindowInsetsCompat
 
 
 class Dashboard_Module : AppCompatActivity() {
+
+    private fun  updateDashboardTotals()
+    {
+        val totalExpenses = ExpenseRepository.expenses.sumOf{ it.amountAdded }
+        val totalGoals = GoalRepository.goals.sumOf { it.savedAmount }
+        val totalChallenges = ChallengeRepository.challenges.sumOf { it.amountSaved }
+
+        val net = totalGoals + totalChallenges - totalExpenses
+
+
+    findViewById<TextView>(R.id.txtDashboardExpenses).text = "Expenses: R$totalExpenses"
+
+        findViewById<TextView>(R.id.txtDashboardGoals).text = "Goals saved : R$totalGoals"
+
+        findViewById<TextView>(R.id.txtDashboardChallenges).text = "Challenges Saved : R$totalChallenges"
+
+        findViewById<TextView>(R.id.txtNetBalance).text = "Net Balance : R%d".format(net)
+    }
+
+    override fun onResume(){
+        super.onResume()
+        updateDashboardTotals()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_dashboard_module)
+        updateDashboardTotals()
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)

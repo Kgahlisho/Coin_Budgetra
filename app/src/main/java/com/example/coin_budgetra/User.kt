2cbc91b8888d8a705071edcbccf2c09cf1e3dcf3
Kgahlisho <st10325173@rcconnect.edu.za>
2026-04-18 00:53:13 +0200

    The user repository and the user data class will be used as temp storages to store user data that will be used on the dashboard and validation confirmation

---
+++ app/src/main/java/com/example/coin_budgetra/User.kt
@@ -0,0 +1,10 @@
+package com.example.coin_budgetra
+
+data class User (
+    val name : String,
+    val surname : String,
+    val phone : String,
+    val email : String,
+    val password : String
+
+)
---
+++ app/src/main/java/com/example/coin_budgetra/UserRepository.kt
@@ -0,0 +1,7 @@
+package com.example.coin_budgetra
+
+object UserRepository {
+    val users = mutableListOf<User>()
+
+var currentUser: User? = null
+}
